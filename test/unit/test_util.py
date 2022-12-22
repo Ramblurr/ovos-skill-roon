e313d36f3d25b4d892609528d4d0c433a1c70f2c
Casey <unnamedrambler@gmail.com>
2022-12-22 11:52:56 +0100

    Improve fuzzy matcher
    Using fuzz.WRation is important because it sends a higher signal as it
    will compare short and long strings differently.

---
+++ test/unit/test_util.py
@@ -0,0 +1,19 @@
+import pytest
+import unittest
+from fuzzywuzzy import fuzz, process as fuzz_process
+
+import util
+
+class TestMatchOne(unittest.TestCase):
+    def test_match_one(self):
+        playlists = [
+            {"title": "Malika Sellami",},
+            {"title": "The Balkan Gypsy Vinyl Boxes  (all editions)  presented by Dunkelbunt",},
+            {"title": "Something about balkans but not gypsys"},
+            {"title": "Balkan Gypsy"},
+            {"title": "Balkan Gypsy (2023 remix edition)"},
+        ]
+        r, _ = util.match_one("balkan gypsy", playlists, "title")
+        self.assertEqual(r["title"], "Balkan Gypsy")
+        r, _ = util.match_one("balkan gypsy remix edition", playlists, "title")
+        self.assertEqual(r["title"], "Balkan Gypsy (2023 remix edition)")
--- util.py
+++ util.py
@@ -1,14 +1,49 @@
-from fuzzywuzzy import fuzz, process as fuzz_process
-from typing import Any, Dict, List, Optional, Tuple
+"""Utils."""
 
-def match_one(input, choices, key) -> Tuple[Optional[Dict], int]:
+import re
+from mycroft.util.parse import fuzzy_match
+from fuzzywuzzy import fuzz, process as fuzz_process
+from fuzzywuzzy import utils
+from typing import Any, Dict, List, Optional, Tuple
+from fuzzysearch import find_near_matches
+
+
+default_processor = utils.full_process
+
+def expand_choices(choices, key, key2):
+    """Expand choices to include a variant with content in parens removed."""
+    new_choices = choices.copy()
+    for choice in choices:
+        orig = choice.get(key, "")
+        choice_stripped = re.sub('(\(.+\)|-.+)$', '', orig).strip()
+        if orig != choice_stripped:
+            choice2 = choice.copy()
+            choice2[key2] = choice_stripped
+            new_choices.append(choice2)
+    return new_choices
+
+
+
+def key_processor(key, key2):
+    """Process, for fuzzywuzzy, that uses the key to extract the string."""
+    def processor(s):
+        if isinstance(s, dict):
+            if key2 in s:
+                return utils.full_process(s[key2])
+            return utils.full_process(s.get(key, ""))
+        return utils.full_process(s)
+    return processor
+
+
+def match_one(query, choices, key) -> Tuple[Optional[Dict], int]:
     """Match a single item from a list of choice dicts."""
-    options = [i.get(key, "").lower() for i in choices]
+    choices = expand_choices(choices, key, "_expanded")
     try:
-        opt, confidence = fuzz_process.extractOne(
-            input, options, scorer=fuzz.ratio
-        )
-        choice = [i for i in choices if i.get(key, "").lower() == opt][0]
-        return choice, confidence
-    except:
+        best_list = fuzz_process.extract(query, choices, scorer=fuzz.WRatio, processor=key_processor(key, "_expanded"), limit=999)
+        chosen, confidence = max(best_list, key=lambda i: i[1])
+        if "_expanded" in chosen:
+            del chosen["_expanded"]
+        return chosen, confidence/100
+    except Exception as e:
+        #raise e
         return None, 0
