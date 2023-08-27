e23419431c6bfb1126f187bc193b50d3b0a0d07c
Casey Link <unnamedrambler@gmail.com>
2023-08-27 17:45:16 +0200

    Add ci

--- docker-dev/entrypoint.sh
+++ docker-dev/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 set -ex
 
+pip3 install -e /mycroft_roon_skill
+
 COMMAND=${1:-"both"}
 
 if [ "$COMMAND" = "server" ]; then
     /home/ovos/.venv/bin/roon-proxy-server
 elif [ "$COMMAND" = "skill" ]; then
-    /home/ovos/.venv/bin/ovos-skill-launcher mycroft-roon-skill.ramblurr
+    /home/ovos/.venv/bin/ovos-skill-launcher mycroft-roon-skill.ramblurr /mycroft_roon_skill
 else
     /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 fi
