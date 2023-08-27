1daecbf8b1d598cc5cc273585622cdd3210b6fd8
Casey Link <unnamedrambler@gmail.com>
2023-08-27 20:43:45 +0200

    Update docker build

---
+++ docker/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+VERSION=dev
+OVOS_USER=ovos
+TMP_FOLDER=~/src/ovos/data/tmp
+CONFIG_FOLDER=~/src/ovos/data/config
+SKILL_FOLDER=~/src/mycroft/roon-skill
+
+
+podman run --userns=keep-id \
+    -v ${SKILL_FOLDER}:/skill_roon:Z \
+    -v ${CONFIG_FOLDER}:/home/${OVOS_USER}/.config/mycroft:Z \
+    -v ${TMP_FOLDER}:/tmp/mycroft:Z \
+    -it \
+    --name ramblurr_skill_roon \
+    --rm \
+    --network=host \
+    localhost/roon-test:latest
+    #--entrypoint=/bin/sh \
